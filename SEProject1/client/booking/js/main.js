c781c32fc8234b31755d218076404469c4939804
Mark Zoiku <mark.zoiku@ashesi.edu.gh>
2021-02-26 14:05:06 +0000

    Userbooking API updated + Userbooking (main.js) updated

--- SEProject1/client/booking/js/main.js
+++ SEProject1/client/booking/js/main.js
@@ -1,14 +1,34 @@
-(function($) {
+// (function($) {
 
-    $(".toggle-password").click(function() {
+//     $(".toggle-password").click(function() {
 
-        $(this).toggleClass("zmdi-eye zmdi-eye-off");
-        var input = $($(this).attr("toggle"));
-        if (input.attr("type") == "password") {
-          input.attr("type", "text");
-        } else {
-          input.attr("type", "password");
-        }
-      });
-})(jQuery);
+//         $(this).toggleClass("zmdi-eye zmdi-eye-off");
+//         var input = $($(this).attr("toggle"));
+//         if (input.attr("type") == "password") {
+//           input.attr("type", "text");
+//         } else {
+//           input.attr("type", "password");
+//         }
+//       });
+// })(jQuery);
 
+$(document).ready(()=> {
+  var apiUrl = "https://restcountries.eu/rest/v2/all";
+  $.get(apiUrl,
+      data => {
+          data.forEach(country => {
+              $('#Nationality').append(`<option value='${country.name}'>${country.name}</option>`)
+          });
+      }
+  );
+
+  $('.insurance').css("display", "none");
+
+  $("#No").click(()=>{
+    $('.insurance').css("display", "none");
+  });
+
+  $("#Yes").click(()=>{
+    $(".insurance").css("display", "inline");
+  });
+});
