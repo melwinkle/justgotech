f350d977221e026f2e3c27023e5eb106e078025b
Hephzibah Emereole <hephzibah.emereole@ashesi.edu.gh>
2021-04-06 18:26:09 +0000

    Fixing booking error-cannot insert into db

--- SEProject1/booking2/bookingverify.php
+++ SEProject1/booking2/bookingverify.php
@@ -4,7 +4,7 @@
  * This file is used to verify the booking details  of the user. It includes the  database file and the form in booking.php 
  * If the user input is correctly filled in, it inserts it into the database.
  */
-    // require_once('booking.php');
+    require_once('booking.php');
     // // require('connection.php'); 
 
 
@@ -25,9 +25,7 @@
      
     if(isset($_POST['submit'])){
         $patient = $_POST['patient'];
-        echo "v1" . $patient  . "<br>";
         $doctor = $_POST['doctor'];
-        echo "v1" . $doctor. "<br>";
         $healthInsu = $_POST['healthIn'];
         $InName = $_POST['Insurname'];
         $InID = $_POST['InsurId'];
@@ -35,25 +33,33 @@
         $Appdate = $_POST['appDate'];
         $Apptime = $_POST['apptime'];
         $Departm = $_POST['departm'];
+
+        $sql = "INSERT INTO `booking`(`Appointment`, `Department`, `Reason`, `Insurance`, `Insurance_Name`, `Insurance_ID`, `Doctor`, `ApTime`, `Person`, `PatientID`)
+         VALUES ('2021-04-15' ,'Pediatrics' ,'sdfcc' ,'Yes' ,'ergcggccc' ,'445452' ,'Dr.Rita' ,'18:08' ,'Self', '1')";
+
+        $res1 = mysqli_query($conn, $sql);
+          
  
         //the sql inserts into the database columns
-        $sql = "INSERT INTO booking (Appointment, Department,Reason,Insurance,Insurance_Name,Insurance_ID,Doctor,ApTime, Person, PatientID) 
-         VALUES($Appdate, $Departm, $BookingR, $healthInsu, $InName, $InID, $doctor, $Apptime, $patient, 1)";
+        // $sql = "INSERT INTO booking (Appointment, Department,Reason,Insurance,Insurance_Name,Insurance_ID,Doctor,ApTime, Person, PatientID) 
+        //  VALUES($Appdate, $Departm, $BookingR, $healthInsu, $InName, $InID, $doctor, $Apptime, $patient, 1)";
             
         // $sql2 = "INSERT INTO `booking` (`BID`, `Appointment`, `Department`, `Reason`, `Insurance`, `Insurance_Name`, `Insurance_ID`, `Image`, `Doctor`, `ApTime`, `Person`, `PatientID`) 
         //VALUES ('2', '23/04/2021', 'Optics', 'fgfgbcbcncncn', 'Yes', 'ghghncncmccckkc', '562902', 'img1.png')";
         
-            $results = mysqli_query($conn, $sql); 
-            var_dump($results);
-            if ($results) {
-                echo "record inserted";
-                header("Location: ../client/tracker/tracker.php");
-             }
+        //     $results = mysqli_query($conn, $sql); 
+        //     //var_dump($results);
+        //     if ($results) {
+        //         echo "record inserted";
+        //         header("Location: ../client/tracker/tracker.php");
+        //      }
             
-            else {
-               echo "record not inserted";
+        //     else {
+        //        echo "xxxxxxxxxxxxx" .$results; 
                
-            }
-        }
-        mysqli_close($conn);
+        //     }
+         }
+
+        //mysqli_close($conn);
+
     ?>
