020c75d467e89307dcdd72e9dd1777000fef9658
Hephzibah Emereole <hephzibah.emereole@ashesi.edu.gh>
2021-04-12 16:31:56 +0000

    Testing in progress

--- SEProject1/client/booking2/booking.php
+++ SEProject1/client/booking2/booking.php
@@ -10,7 +10,6 @@ if(!isset($_SESSION['username'])){
 }
 
 $username=$_SESSION['username'];
-$userID = $_SESSION['patientID'];
 
 $query="SELECT * from customer where username='$username'";
 $result=mysqli_query($conn,$query);
