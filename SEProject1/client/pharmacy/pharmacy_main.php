cf1837c111ac6a63a01c45b09ab2c8ff5bd53708
Aileen Akpalu <adzo.lisa@gmail.com>
2021-04-26 21:32:01 +0000

    updated

--- SEProject1/client/pharmacy/pharmacy_main.php
+++ SEProject1/client/pharmacy/pharmacy_main.php
@@ -73,50 +73,18 @@ $countp=0;
 </div>
 
 <div class="opt" style="margin-bottom: 20px">
-<button class="main" onclick="mainP()" style="height:30%">
-    <div class="main_img">
-        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="500" height="500" viewBox="0 0 959 615">
-         <defs>
-     <linearGradient id="linear-gradient" x2="1" y2="0.517" gradientUnits="objectBoundingBox">
-      <stop offset="0" stop-color="#3e4ccb"/>
-      <stop offset="1" stop-color="#289697"/>
-     </linearGradient>
-         </defs>
-        <g id="Group_23" data-name="Group 23" transform="translate(-203 -185)">
-     <g id="Rectangle_13" data-name="Rectangle 13" transform="translate(203 185)" stroke="#b720e5" stroke-width="5" fill="url(#linear-gradient)">
-      <rect width="959" height="615" rx="15" stroke="none"/>
-      <rect x="2.5" y="2.5" width="954" height="610" rx="12.5" fill="none"/>
-     </g>
-     <text id="MAIN_STORE" data-name="MAIN STORE" transform="translate(254 518)" fill="#fff" font-size="70" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="0" y="0">MAIN STORE</tspan></text>
-     <path id="Icon_awesome-shopping-cart" data-name="Icon awesome-shopping-cart" d="M307.746,158.293,335.293,49.024c1.989-7.889-4.663-15.4-13.637-15.4H92.774l-5.341-23.54C86.1,4.214,80.375,0,73.731,0H13.985C6.261,0,0,5.645,0,12.608v8.405c0,6.963,6.261,12.608,13.985,12.608H54.707L95.642,214.039c-9.793,5.077-16.392,14.6-16.392,25.513,0,16.248,14.61,29.419,32.632,29.419s32.632-13.171,32.632-29.419a27.993,27.993,0,0,0-9.8-21.013H256.876a27.994,27.994,0,0,0-9.8,21.013c0,16.248,14.61,29.419,32.632,29.419s32.632-13.171,32.632-29.419c0-11.648-7.51-21.713-18.4-26.479l3.215-12.753c1.989-7.889-4.663-15.4-13.637-15.4H127.1l-3.814-16.811H294.109C300.639,168.107,306.3,164.033,307.746,158.293Z" transform="translate(729.947 356.015)" fill="#fdfafa"/>
-         </g>
-        </svg>
-</div>
+<button class="main" onclick="mainP()" style="width:40%;background:rgb(23, 79, 140);height:300px;border-radius:10px;border:3px black;color:white;font-size:24pt">
+MAIN STORE
 </button>
-<button class="purchase" onclick="purP()" style="height:30%">
-    <div class="main_img">
-    <svg id="Component_4_2" data-name="Component 4 – 2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="500" height="500" viewBox="0 0 959 615">
-  <defs>
-    <linearGradient id="linear-gradient" x2="1" y2="0.517" gradientUnits="objectBoundingBox">
-      <stop offset="0" stop-color="#3e4ccb"/>
-      <stop offset="1" stop-color="#289697"/>
-    </linearGradient>
-  </defs>
-  <g id="Rectangle_13" data-name="Rectangle 13" stroke="#b720e5" stroke-width="5" fill="url(#linear-gradient)">
-    <rect width="959" height="615" rx="15" stroke="none"/>
-    <rect x="2.5" y="2.5" width="954" height="610" rx="12.5" fill="none"/>
-  </g>
-  <text id="PURCHASES" transform="translate(142 335)" fill="#fff" font-size="70" font-family="Arial-Black, Arial Black" font-weight="800"><tspan x="0" y="0">PURCHASES</tspan></text>
-  <path id="Icon_material-attach-money" data-name="Icon material-attach-money" d="M93.248,83.166C58.548,77.291,47.39,71.216,47.39,61.757c0-10.854,15.439-18.422,41.272-18.422,27.209,0,37.3,8.464,38.215,20.911H160.66c-1.07-17.127-17.12-32.86-49.068-37.939V4.5H65.733V26.009c-29.655,4.182-53.5,16.729-53.5,35.947,0,23,29.2,34.454,71.845,41.125,38.215,5.975,45.858,14.737,45.858,24,0,6.871-7.49,17.824-41.272,17.824-31.489,0-43.871-9.161-45.553-20.911H9.48c1.834,21.807,26.9,34.055,56.253,38.138v21.608h45.858V162.329c29.808-3.684,53.5-14.936,53.5-35.35C165.093,98.7,127.947,89.041,93.248,83.166Z" transform="translate(650.52 213.381)" fill="#faf5f5"/>
-</svg>
-</div>
+<button class="purchase" onclick="purP()" style="width:40%;background:rgb(23, 79, 110);height:300px;border-radius:10px;border:3px black;color:white;font-size:24pt">
+PURCHASES
 </button>
 
 
 
 </div>
-<div style="margin-left:40%;margin-top: 85px;">
-  <button onclick="track()"style="width:55%;background:skyblue;height:300px">TRACK ORDERS</buttom>
+<div style="margin-left:35%;margin-top: 40px;">
+  <button onclick="track()"style="width:55%;background:rgb(23, 79, 95);height:300px;border-radius:10px;border:3px black;color:white;font-size:24pt">TRACK ORDERS</buttom>
 </div>
 
    
