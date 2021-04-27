1d172cf3b247684ca27073339bd28fa66b100722
Hephzibah Emereole <hephzibah.emereole@ashesi.edu.gh>
2021-04-27 21:22:34 +0000

    Update

--- SEProject1/payment/index.php
+++ SEProject1/payment/index.php
@@ -21,39 +21,20 @@ if(!isset($_SESSION['username'])){
 <html>
 <head>
 <title>Just Go Tech Payment</title>
-<!-- for-mobile-apps -->
 
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="Payment Form Widget Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
-Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
-		function hideURLbar(){ window.scrollTo(0,1); } </script> -->
-<!-- //for-mobile-apps -->
 
 
 <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
-<!-- <link href='//fonts.googleapis.com/css?family=Fugaz+One' rel='stylesheet' type='text/css'>
-<link href='//fonts.googleapis.com/css?family=Alegreya+Sans:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,800,800italic,900,900italic' rel='stylesheet' type='text/css'>
-<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'> -->
-<!-- <script type="text/javascript" src="js/jquery.min.js"></script> -->
 </head>
 <body>
 	<div class="main">
 		<h1>Payment  Details</h1>
-		<div class="content">
-			<div class="sap_tabs">
-				<div id="horizontalTab" style="display: block; width: 100%; margin: 0px;">
-					<div class="pay-tabs">
 						<h2>Welcome to Just Go Tech Payment Portal </h2>
-
-                         <div class="payment-info">
-                            <a href = "https://ravesandbox.flutterwave.com/pay/justgotechtrial"  style ="align: center">PROCEED </a>
+                        <div>
+                            <button> <a href = "https://ravesandbox.flutterwave.com/pay/justgotechtrial" > PROCEED </a> </button>
            		        </div>
-					    </div>
-                     </div>
-                    </div>
-                </body>
+                    </body>
             </html>
 
                 
