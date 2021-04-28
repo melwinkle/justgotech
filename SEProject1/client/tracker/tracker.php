51f0a5a59c868b7dc33071bfe4ea3736984bec12
Aileen Akpalu <adzo.lisa@gmail.com>
2021-04-28 23:43:13 +0000

    updated

--- SEProject1/client/tracker/tracker.php
+++ SEProject1/client/tracker/tracker.php
@@ -67,9 +67,13 @@ $countp=0;
 
 <div class="navb"id="main">
   <span style="font-size:30px;cursor:pointer" onclick="openNav()"><img style="width:10%" src="../../images/justgo.png" alt="justgotech"> </span>
-  
-  <span style="font-size:20px;cursor:pointer; float:right; margin-right: -32%" onclick="openP()"><?php echo $row['firstname']." " .$row['lastname'];?><img style="width:10%" src="../../images/stethoscope.png" alt="profile"> </span>
-
+ 
+  <div style="float:right">
+  <button style="background:none;border:none;margin-left:68%"><img src="https://img.icons8.com/metro/26/000000/filled-message.png"/></button>
+  <button style="background:none;border:none;margin-left:-8%"><img style="width:25%"src="https://img.icons8.com/plasticine/100/000000/appointment-reminders.png"/><img style="width:10%"src="https://img.icons8.com/ios-filled/50/e74c3c/2-circle.png"/></button>
+ <span style="font-size:20px;cursor:pointer;margin-left:-5% " onclick="openP()"><?php echo $row['firstname']." " .$row['lastname'];?><img style="width:5%" src="../../images/stethoscope.png" alt="profile"> </span>
+ 
+ </div>
 </div>
 
 
