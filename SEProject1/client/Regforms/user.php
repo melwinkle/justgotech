ea215412b4eb00bbf0ce5c6b5b2cf18a0e15065b
“mark <mark.zoiku@ashesi.edu.gh>
2021-05-09 22:16:42 +0000

    login soemthing

--- SEProject1/client/Regforms/user.php
+++ SEProject1/client/Regforms/user.php
@@ -3,9 +3,10 @@
 require_once("../../database/connection.php");
 
     if(!isset($_SESSION['username'])){
-        if(isset($_POST["login"])){
+        // if(isset($_POST["login"])){
             $username = $_POST['q24_username'];
-            $password = md5(sha1($_POST['q29_password']));
+            // $password = md5(sha1($_POST['q29_password']));
+            $password =$_POST['q29_password'];
             $query = "SELECT * from customer WHERE username = '$username' and  userpassword = '$password'";
             $sql = mysqli_query($conn, $query);
             $results = mysqli_num_rows($sql);
@@ -19,8 +20,8 @@ require_once("../../database/connection.php");
                 header("Location: ../Regforms/login.html?details=false");
             }
            
-        }
-        header("Location: ../Regforms/login.html?login=false");
+        // }
+        // header("Location: ../Regforms/login.html?login=false");
     }
 
 
