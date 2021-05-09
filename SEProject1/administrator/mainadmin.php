420bd7a24ce533dc501c78eba7c3a28fdda64125
Aileen Akpalu <adzo.lisa@gmail.com>
2021-05-09 14:55:02 +0000

    admin

---
+++ SEProject1/administrator/mainadmin.php
@@ -0,0 +1,171 @@
+<?php require_once("/Applications/XAMPP/xamppfiles/htdocs/justgotech/SEProject1/database/connection.php");?>
+<!DOCTYPE html>
+
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="DASHBOARD, Total Unique Visits, 200, Total Unique Visits, 200">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="mainadmin.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 3.13.2, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"url": "index.html",
+		"logo": "images/justgotech.png"
+}</script>
+    <meta property="og:title" content="Home">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#478ac9">
+    <link rel="canonical" href="index.html">
+    <meta property="og:url" content="index.html">
+  </head>
+  <body data-home-page="Home.html" data-home-page-title="Home" class="u-body"><header class="u-clearfix u-header u-header" id="sec-3c0d"><div class="u-clearfix u-sheet u-sheet-1">
+        <a href="https://nicepage.com" class="u-image u-logo u-image-1" data-image-width="574" data-image-height="435">
+          <img src="images/justgotech.png" class="u-logo-image u-logo-image-1" data-image-width="64">
+        </a>
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><symbol id="menu-hamburger" viewBox="0 0 16 16" style="width: 16px; height: 16px;"><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</symbol>
+</defs></svg>
+            </a>
+          </div>
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="mainadmin.html" style="padding: 10px 20px;">Dashboard</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Logout.html" style="padding: 10px 20px;">Logout</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" style="padding: 10px 20px;">Add New Admin</a>
+
+</li></ul>
+          </div>
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="mainadmin.html" style="padding: 10px 20px;">Dashboard</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Services.html" style="padding: 10px 20px;">Services</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Logout.html" style="padding: 10px 20px;">Logout</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" style="padding: 10px 20px;">Add New Admin</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+      </div></header>
+    <section class="u-align-center u-clearfix u-color-scheme-custom-color-scheme-1 u-color-style-multicolor-1 u-palette-1-base u-section-1" id="sec-28e9">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h1 class="u-text u-text-1">Dashboard</h1>
+        <div class="u-expanded-width u-list u-list-1">
+          <div class="u-repeater u-repeater-1">
+            <div class="u-align-center u-border-2 u-border-white u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-container-layout-1"><span class="u-icon u-icon-circle u-icon-1"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 512 512" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-a3e3"></use></svg><svg class="u-svg-content" viewBox="0 0 512 512" id="svg-a3e3"><path d="m157 453.378h-27.333c-5.523 0-10-4.478-10-10s4.478-10 10-10h27.333c5.523 0 10 4.478 10 10s-4.477 10-10 10z"></path><path d="m232.111 453.378h-27.334c-5.522 0-10-4.478-10-10s4.478-10 10-10h27.334c5.523 0 10 4.478 10 10s-4.477 10-10 10z"></path><path d="m307.223 453.378h-27.334c-5.522 0-10-4.478-10-10s4.478-10 10-10h27.334c5.522 0 10 4.478 10 10s-4.478 10-10 10z"></path><path d="m382.333 453.378h-27.333c-5.522 0-10-4.478-10-10s4.478-10 10-10h27.333c5.522 0 10 4.478 10 10s-4.478 10-10 10z"></path><path d="m255.819 512c-4.233 0-8.084-2.765-9.462-6.763-1.363-3.954-.037-8.474 3.248-11.067 3.242-2.559 7.805-2.844 11.352-.739 3.58 2.125 5.489 6.439 4.663 10.519-.934 4.616-5.079 8.05-9.801 8.05z"></path><path d="m441.964 512h-140.758c-5.522 0-10-4.478-10-10s4.478-10 10-10h130.758v-97.244h-351.928v97.244h130.758c5.523 0 10 4.478 10 10s-4.477 10-10 10h-140.758c-5.522 0-10-4.478-10-10v-117.244c0-5.522 4.478-10 10-10h371.928c5.522 0 10 4.478 10 10v117.244c0 5.522-4.478 10-10 10z"></path><path d="m314.616 173.428c23.1-17.675 38.025-45.521 38.025-76.788 0-53.287-43.353-96.64-96.641-96.64s-96.641 43.353-96.641 96.641c0 31.267 14.925 59.113 38.025 76.788-44.398 2.606-79.719 39.55-79.719 84.59v59.586c0 5.523 4.478 10 10 10h256.668c5.522 0 10-4.477 10-10v-59.586c.001-45.04-35.32-81.985-79.717-84.591zm-135.257-76.787c0-42.26 34.381-76.641 76.641-76.641s76.641 34.381 76.641 76.641-34.381 76.641-76.641 76.641-76.641-34.382-76.641-76.641zm194.975 210.963h-236.668v-49.586c0-35.696 29.041-64.737 64.737-64.737h107.193c35.696 0 64.737 29.041 64.737 64.737v49.586z"></path></svg>
+            
+            <?php 
+            //counts number of patientids and sets them to the user variable which is displayed
+            $sql="select count(PatientId) AS 'users' from Customer";
+			      $stmt= mysqli_stmt_init($conn);
+			      if(!mysqli_stmt_prepare($stmt,$sql)){
+			    	echo 'sql error1';
+					
+			      	exit();
+			        }else{
+					
+				    mysqli_stmt_execute($stmt);
+				    $result= mysqli_stmt_get_result($stmt);
+				    while($row=mysqli_fetch_array($result)){
+					  $users=$row['users'];
+                }
+            
+					  
+              }
+            //counts number of bookings as active users and sets it to a variable which is displayed.
+            $sql="select count(BID) AS 'active_users' from booking";
+			      $stmt= mysqli_stmt_init($conn);
+			      if(!mysqli_stmt_prepare($stmt,$sql)){
+			    	echo 'sql error2';
+					
+			      	exit();
+			        }else{
+					
+				    mysqli_stmt_execute($stmt);
+				    $result= mysqli_stmt_get_result($stmt);
+				    while($row=mysqli_fetch_array($result)){
+					  $active_users=$row['active_users'];
+                }
+            
+					  
+              }
+            //I havent dones one for the purchases because I cant find the information to draw from. I dont know how to record 
+            //the number of purchases
+            
+            
+            ?>
+          </span>
+                <h1 class="u-text u-title u-text-2" data-animation-name="counter" data-animation-event="scroll" data-animation-duration="3000">15</h1>
+                <h1 class="u-text u-text-3">No of Registered Users</h1>
+              </div>
+            </div>
+            <div class="u-align-center u-border-2 u-border-white u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-container-layout-2"><span class="u-icon u-icon-circle u-icon-2">
+            <svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 60 60" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-957d"></use></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" xml:space="preserve" class="u-svg-content" viewBox="0 0 60 60" x="0px" y="0px" id="svg-957d" style="enable-background:new 0 0 60 60;"><path d="M48.014,42.889l-9.553-4.776C37.56,37.662,37,36.756,37,35.748v-3.381c0.229-0.28,0.47-0.599,0.719-0.951
+	c1.239-1.75,2.232-3.698,2.954-5.799C42.084,24.97,43,23.575,43,22v-4c0-0.963-0.36-1.896-1-2.625v-5.319
+	c0.056-0.55,0.276-3.824-2.092-6.525C37.854,1.188,34.521,0,30,0s-7.854,1.188-9.908,3.53C17.724,6.231,17.944,9.506,18,10.056
+	v5.319c-0.64,0.729-1,1.662-1,2.625v4c0,1.217,0.553,2.352,1.497,3.109c0.916,3.627,2.833,6.36,3.503,7.237v3.309
+	c0,0.968-0.528,1.856-1.377,2.32l-8.921,4.866C8.801,44.424,7,47.458,7,50.762V54c0,4.746,15.045,6,23,6s23-1.254,23-6v-3.043
+	C53,47.519,51.089,44.427,48.014,42.889z M51,54c0,1.357-7.412,4-21,4S9,55.357,9,54v-3.238c0-2.571,1.402-4.934,3.659-6.164
+	l8.921-4.866C23.073,38.917,24,37.354,24,35.655v-4.019l-0.233-0.278c-0.024-0.029-2.475-2.994-3.41-7.065l-0.091-0.396l-0.341-0.22
+	C19.346,23.303,19,22.676,19,22v-4c0-0.561,0.238-1.084,0.67-1.475L20,16.228V10l-0.009-0.131c-0.003-0.027-0.343-2.799,1.605-5.021
+	C23.253,2.958,26.081,2,30,2c3.905,0,6.727,0.951,8.386,2.828c1.947,2.201,1.625,5.017,1.623,5.041L40,16.228l0.33,0.298
+	C40.762,16.916,41,17.439,41,18v4c0,0.873-0.572,1.637-1.422,1.899l-0.498,0.153l-0.16,0.495c-0.669,2.081-1.622,4.003-2.834,5.713
+	c-0.297,0.421-0.586,0.794-0.837,1.079L35,31.623v4.125c0,1.77,0.983,3.361,2.566,4.153l9.553,4.776
+	C49.513,45.874,51,48.28,51,50.957V54z"></path></svg>
+          </span>
+                <h1 class="u-text u-title u-text-4" data-animation-name="counter" data-animation-event="scroll" data-animation-duration="3000">20</h1>
+                <h1 class="u-text u-text-5">Number of Active Users</h1>
+              </div>
+            </div>
+            <div class="u-align-center u-border-2 u-border-white u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-container-layout-3"><span class="u-icon u-icon-circle u-icon-3"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 511.728 511.728" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-21c3"></use></svg><svg class="u-svg-content" viewBox="0 0 511.728 511.728" id="svg-21c3"><path d="m147.925 379.116c-22.357-1.142-21.936-32.588-.001-33.68 62.135.216 226.021.058 290.132.103 17.535 0 32.537-11.933 36.481-29.017l36.404-157.641c2.085-9.026-.019-18.368-5.771-25.629s-14.363-11.484-23.626-11.484c-25.791 0-244.716-.991-356.849-1.438l-17.775-65.953c-4.267-15.761-18.65-26.768-34.978-26.768h-56.942c-8.284 0-15 6.716-15 15s6.716 15 15 15h56.942c2.811 0 5.286 1.895 6.017 4.592l68.265 253.276c-12.003.436-23.183 5.318-31.661 13.92-8.908 9.04-13.692 21.006-13.471 33.695.442 25.377 21.451 46.023 46.833 46.023h21.872c-3.251 6.824-5.076 14.453-5.076 22.501 0 28.95 23.552 52.502 52.502 52.502s52.502-23.552 52.502-52.502c0-8.049-1.826-15.677-5.077-22.501h94.716c-3.248 6.822-5.073 14.447-5.073 22.493 0 28.95 23.553 52.502 52.502 52.502 28.95 0 52.503-23.553 52.503-52.502 0-8.359-1.974-16.263-5.464-23.285 5.936-1.999 10.216-7.598 10.216-14.207 0-8.284-6.716-15-15-15zm91.799 52.501c0 12.408-10.094 22.502-22.502 22.502s-22.502-10.094-22.502-22.502c0-12.401 10.084-22.491 22.483-22.501h.038c12.399.01 22.483 10.1 22.483 22.501zm167.07 22.494c-12.407 0-22.502-10.095-22.502-22.502 0-12.285 9.898-22.296 22.137-22.493h.731c12.24.197 22.138 10.208 22.138 22.493-.001 12.407-10.096 22.502-22.504 22.502zm74.86-302.233c.089.112.076.165.057.251l-15.339 66.425h-51.942l8.845-67.023 58.149.234c.089.002.142.002.23.113zm-154.645 163.66v-66.984h53.202l-8.84 66.984zm-74.382 0-8.912-66.984h53.294v66.984zm-69.053 0h-.047c-3.656-.001-6.877-2.467-7.828-5.98l-16.442-61.004h54.193l8.912 66.984zm56.149-96.983-9.021-67.799 66.306.267v67.532zm87.286 0v-67.411l66.022.266-8.861 67.145zm-126.588-67.922 9.037 67.921h-58.287l-18.38-68.194zm237.635 164.905h-36.426l8.84-66.984h48.973l-14.137 61.217c-.784 3.396-3.765 5.767-7.25 5.767z"></path></svg>
+            
+            
+          </span>
+                <h1 class="u-text u-title u-text-6" data-animation-name="counter" data-animation-event="scroll" data-animation-duration="3000">+300</h1>
+                <h1 class="u-text u-text-7">No of Purchases Made</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-0605"><div class="u-clearfix u-sheet u-sheet-1">
+        <p class="u-small-text u-text u-text-variant u-text-1">Just Go 2021</p>
+      </div></footer>
+    <section class="u-backlink u-clearfix u-grey-80">
+      <a class="u-link" href="https://nicepage.com/website-templates" target="_blank">
+        <span>Website Templates</span>
+      </a>
+      <p class="u-text">
+        <span>created with</span>
+      </p>
+      <a class="u-link" href="https://nicepage.com/" target="_blank">
+        <span>Website Builder Software</span>
+      </a>. 
+    </section>
+  </body>
+</html>
