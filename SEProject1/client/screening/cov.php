bc66d52679a86bf866cc11564df7d573fcc3f351
Aileen Akpalu <adzo.lisa@gmail.com>
2021-05-12 14:14:58 +0000

    updated

--- SEProject1/client/screening/cov.php
+++ SEProject1/client/screening/cov.php
@@ -58,7 +58,7 @@ $resultss=mysqli_query($conn,$query2);
 <div class="navb"id="main">
   <span style="font-size:30px;cursor:pointer" onclick="openNav()"><img style="width:10%" src="../../images/justgo.png" alt="justgotech"> </span>
   
-  <span style="font-size:20px;cursor:pointer; float:right; margin-right: -32%" onclick="openP()"><?php echo $row['firstname']." " .$row['lastname'];?><img style="width:10%" src="/justgotech/SEProject1/images/stethoscope.png" alt="profile"> </span>
+  <span style="font-size:20px;cursor:pointer; float:right; margin-right: -22%" onclick="openP()"><?php echo $row['firstname']." " .$row['lastname'];?><img style="width:10%" src="/justgotech/SEProject1/images/stethoscope.png" alt="profile"> </span>
 
 </div>
    
